b65fd9f464d2c33e6d9bf43b39c98d3cb58e355e
Steve Shilling <steve@therapypages.com>
2018-03-11 13:47:21 +0000

    Directory not required

--- docker/old/app/bin/server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script should be in the same location as the trade-app JAR file
-
-nohup java -Dspring.profiles.active=prod -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9990 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -jar target/trade-app-0.1.0.jar >trade-app.stdout 2>trade-app.stderr &
--- docker/old/app/src/main/java/com/neueda/trade/server/FatalTradeException.java
+++
@@ -1,9 +0,0 @@
-package com.neueda.trade.server;
-
-@SuppressWarnings("serial")
-public class FatalTradeException extends TradeException
-{
-    public FatalTradeException(String msg, Object... objs) {
-        super(String.format(msg, objs));
-    }
-}
